31522e507324222b70afa2a731ce0ecd9a9be6a8
Soham <sohamaich12345@gmail.com>
2024-09-17 23:06:41 +0930

    Experimenting with the menu library.

--- expemental_menu_system.cpp
+++ expemental_menu_system.cpp
@@ -1,5 +1,17 @@
 // This implements a menu for the 
 #include "mmaker.h"
 
-menu Customer; // A menu object from menu class to implement a customer portal
+int main() {
 
+  menu Customer; // A menu object from menu class to implement a customer portal
+
+  
+
+  Customer.menu_head("Select Cuisine");
+
+  Customer.add("Italian");
+  Customer.add("Indian");
+  Customer.add("British");
+
+
+}
