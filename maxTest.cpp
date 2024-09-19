41d52c2104f83537754fec25d8462182d72899b0
Max Howard <max@Maxs-MacBook-Pro.local>
2024-09-19 15:10:10 +0930

    trial

--- maxTest.cpp
+++ maxTest.cpp
@@ -1,16 +1,66 @@
-#include<iostream>
+#include <iostream>
 #include <string>
+#include<vector>
+
+class Ingredient {
+  private:
+    std::string name;
+    double price;
+  public:
+
+  Ingredient(double price, std::string name) : price(price), name(name) {}
+
+  std::string get_name() {
+      return name;
+  }
+
+};
+
+
+
+class Pasta {
+  private:
+    double price;
+    std::string name;
+    std::vector<Ingredient*> ingredientList;
+
+    // pasta exclusive
+    std::string pastaSauce;
+
+  public:
+
+  Pasta() : price(9999999), name("NoName"), pastaSauce("No Sauce")  {}
+
+  Pasta(double price, std::string name, std::string pastaSauce) : price(price), name(name), pastaSauce(pastaSauce) {}
+
+  void addIngredient(Ingredient &i) {
+    ingredientList.push_back(&i);
+  }
+
+  Ingredient* get_ingredient(int index) {
+    return ingredientList[index];
+  }
+
+  std::string get_ingredientName(int index) {
+      return get_ingredient(index)->get_name();
+  }
+
+};
 
 
 int main() {
 
-class pasta {
-  private:
-    int price;
-    std::string name;
-  public:
+Ingredient tomato(2.0, "Tomato");
+Ingredient mushroom(2.0, "Mushroom");
+Ingredient onion(2.0, "Onion");
 
-}
+Pasta yummy(7.5, "Yummy", "Tomato Sauce");
+
+yummy.addIngredient(tomato);
+yummy.addIngredient(mushroom);
+yummy.addIngredient(onion);
+
+  std::cout << yummy.get_ingredientName(2) << std::endl;
 
   return 0;
 }
