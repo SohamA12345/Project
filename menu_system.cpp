22d753354e7a8cca920e8c325c674f964f1a667e
Soham <sohamaich12345@gmail.com>
2024-09-21 18:13:44 +0930

    Added customer menu

--- menu_system.cpp
+++ menu_system.cpp
@@ -7,25 +7,26 @@
 
 int main() {
   menu portal;  // A menu object from menu class to implement a portal.
-  menu Customer; // A menu object to implment a login portal for manager.
+  menu customer; // A menu object to implment a login portal for manager.
 	menu manager;
 
   portal.menu_head("Portal");
 
-  portal.add("Customer");
+  portal.add("customer");
   portal.add("Manager");
   portal.add("Exit");
 
   int portal_choice = portal.display();
 
-	Customer.menu_head("Login/Create an account");
+	customer.menu_head("Login/Create an account");
 
   switch (portal_choice) {
     case 1:
-      std::cout << "Manager\n";
+      std::cout << "customer\n";
+			int customer_choice = customer.display();
       break;
     case 2:
-      std::cout << "Customer\n";
+      std::cout << "Manager\n";
       break;
 		case 3:
 			std::cout << "Exit\n";
