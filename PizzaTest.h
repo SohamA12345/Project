1f216bc57a2fff43a116c04f2cafa721c745f744
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-03 16:49:01 +0930

    a

--- PizzaTest.h
+++ PizzaTest.h
@@ -5,11 +5,11 @@ private:
     // default test with default constructor
     void test_pizza_default() {
         Pizza p;
-        if (p.get_pizzaBase() != "N/A") {
-            cout << "default test for pizza base failed, result unexpected" << endl;
-        }
         if (p.get_pizzaSauce() != "N/A") {
             cout << "default test for pizza sauce failed, result unexpected" << endl;
+        }
+        if (p.get_pizzaBase() != "N/A") {
+            cout << "default test for pizza base failed, result unexpected" << endl;
         }
         if (p.get_item_name() != "N/A") {
             cout << "default test for pizza name failed, result unexpected" << endl;
@@ -25,26 +25,28 @@ private:
     // default test with no ingredient
     void test_pizza_case1() {
         Pizza p1;
-        p1 = Pizza("P", 1.2, 1, "Base1", "Sauce1");
-        if (p1.get_pizzaBase() != "Base1") {
-            cout << "default test for pizza base failed, result unexpected" << endl;
-        }
+        p1 = Pizza("P", 1.2, 1, "Sauce1", "Base1");
         if (p1.get_pizzaSauce() != "Sauce1") {
-            cout << "default test for pizza sauce failed, result unexpected" << endl;
+            cout << "test 1 for pizza sauce failed, result unexpected" << endl;
         }
-        if(p1.get_item_size() != 2) {
-            cout << "test 1 size failed, result unexpected" << endl;
+        if (p1.get_pizzaBase() != "Base1") {
+            cout << "test 1 for pizza base failed, result unexpected" << endl;
         }
-        if(p1.get_item_name() != "Coke") {
-            cout << "test 1 name failed, result unexpected" << endl;
+        if (p1.get_item_size() != 1) {
+            cout << "test 1 for pizza size failed, result unexpected" << endl;
         }
-        if(p1.get_item_price() != 2.2) {
-            cout << "test 1 price failed, result unexpected" << endl;
+        if (p1.get_item_name() != "P") {
+            cout << "test 1 for pizza name failed, result unexpected" << endl;
         }
-        if(p1.calculate_item_price() != 2.75) {
-            cout << "test 1 price calculation failed, result unexpected" << endl;
+        if (p1.get_item_price() != 1.2) {
+            cout << "test 1 for pizza price failed, result unexpected" << endl;
+        }
+        if (p1.calculate_item_price() != 1.2) {
+            cout << "test 1 for pizza price calculation failed, result unexpected" << endl;
         }
     }
+
+    // test 2 
 public:
     void run_tests() {
         test_pizza_default();
