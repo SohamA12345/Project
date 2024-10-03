4ce968a1b2ff4ea8817a49a87fa237b187f70631
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-03 16:03:02 +0930

    A

--- MainTest.cpp
+++ MainTest.cpp
@@ -1,17 +1,20 @@
 #include "DrinkTest.h"
+#include "PizzaTest.h"
 #include "AddToCartTest.h"
 #include "CustomerLoginTest.h"
-
 
 int main() {
     DrinkTest D_T;
     D_T.run_tests();
 
-    AddToCartTest A_T_C_T;
-    A_T_C_T.run_tests();
+    PizzaTest P_T;
+    P_T.run_tests();
 
-    CustomerLoginTest C_L_T;
-    C_L_T.run_tests();
+    // AddToCartTest A_T_C_T;
+    // A_T_C_T.run_tests();
+
+    // CustomerLoginTest C_L_T;
+    // C_L_T.run_tests();
 
     return 0;
 }
