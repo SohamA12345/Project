8aa7029e2124ecac95dd49ce938dc41b249b1967
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-03 15:54:10 +0930

    A

---
+++ PizzaTest.h
@@ -0,0 +1,17 @@
+#include "Pizza.h"
+
+class PizzaTest {
+private:
+    /* data */
+public:
+    PizzaTest(/* args */);
+    ~PizzaTest();
+};
+
+PizzaTest::PizzaTest(/* args */)
+{
+}
+
+PizzaTest::~PizzaTest()
+{
+}
