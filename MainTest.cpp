0f864f8e0e59f8e0c429d97f026115908272cc29
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-10 15:12:06 +1030

    A

--- MainTest.cpp
+++ MainTest.cpp
@@ -1,17 +1,23 @@
 #include "DrinkTest.h"
 #include "PizzaTest.h"
+#include "PastaTest.h"
 // #include "AddToCartTest.h"
 // #include "CustomerLoginTest.h"
 
 int main() {
 
     cout << "This is the test for Drink (no text = no error, everything working as intended): " << endl;
-    DrinkTest D_T;
-    D_T.run_tests();
+    DrinkTest run_drink;
+    run_drink.run_tests();
 
     cout << "\nThis is the test for Pizza: " << endl;
-    PizzaTest P_T;
-    P_T.run_tests();
+    PizzaTest run_pizza;
+    run_pizza.run_tests();
+
+    cout << "\nThis is the test for Pasta: " << endl;
+    PastaTest run_pasta;
+    run_pasta.run_tests();
+
 
     // AddToCartTest A_T_C_T;
     // A_T_C_T.run_tests();
---
+++ PastaTest.h
@@ -0,0 +1,15 @@
+#include "Pasta.h"
+
+class PastaTest {
+private:
+    // default test case for pasta 
+    void test_pasta_default() {
+
+    }
+
+public:
+    void run_tests() {
+        test_pasta_default();
+    }
+};
+
