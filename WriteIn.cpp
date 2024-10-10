d00a1e8f2f581c81cf26a85d306fb098bdc559d0
Max Howard <max@Maxs-MacBook-Pro.local>
2024-10-10 16:12:36 +1030

    lol

--- WriteIn.cpp
+++ WriteIn.cpp
@@ -4,19 +4,20 @@
 WriteIn::WriteIn() {}
 
 // write to a menu item list
-  void WriteIn::write_menu_item_list(std::vector<string> list, std::string name) { // by string vector 
-      std::string file_name = name + ".txt";
-      std::ofstream outFile(file_name);
+void WriteIn::write_menu_item_list(std::vector<string> list,
+                                   std::string name) {  // by string vector
+  std::string file_name = name + ".txt";
+  std::ofstream outFile(file_name);
 
-      int list_length = list.size();
-      for (int i = 0; i < list_length; i++) {
-        outFile << list[i] << std::endl;
-      }
-  } 
-
-  void WriteIn::write_menu_item_list(std::vector<MenuItem> list, std::string name) { // by menu item vector
-
+  int list_length = list.size();
+  for (int i = 0; i < list_length; i++) {
+    outFile << list[i] << std::endl;
   }
+}
+
+void WriteIn::write_menu_item_list(std::vector<MenuItem> list,
+                                   std::string name) {  // by menu item vector
+}
 
 // write to ingredient list text file
 void WriteIn::write_ingredient_list(IngredientList ingredient_list) {
@@ -27,6 +28,72 @@ void WriteIn::write_ingredient_list(IngredientList ingredient_list) {
     outFile << list[i].get_name() << std::endl;
     outFile << list[i].get_price() << std::endl;
   }
+}
+
+// write in menu item: write in name, price and size
+void WriteIn::write_menu_item(MenuItem* menu_item, std::ofstream* out_file) {
+
+  *out_file << menu_item->get_item_name() << std::endl;
+  *out_file << menu_item->get_item_price() << std::endl;
+  int item_size_number = menu_item->get_item_size();
+  std::string item_size;
+  if (item_size_number == 1) {
+    item_size = "small";
+  } else if (item_size_number == 2) {
+    item_size = "medium";
+  } else if (item_size_number == 3) {
+    item_size = "large";
+  } else {
+    item_size = "small";
+  }
+  *out_file << item_size << std::endl;
+}
+
+// write in pasta
+void WriteIn::write_pasta(Pasta pasta) {
+  
+  std::string file_name = pasta.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&pasta, &out_file);
+
+  out_file << pasta.get_pasta_sauce() << std::endl;
+  int no_ingredients = pasta.get_ingredient_list().size();
+  out_file << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    out_file << pasta.get_ingredient(i).get_name() << std::endl;
+  }
+
+  out_file.close();
+}
+
+// write in pizza
+void WriteIn::write_pizza(Pizza pizza) {
+  std::string file_name = pizza.get_item_name() + ".txt";
+  std::ofstream outFile(file_name);  // make write file object
+
+  outFile << pasta.get_item_name() << std::endl;
+  outFile << pasta.get_item_price() << std::endl;
+  int item_size_number = pasta.get_item_size();
+  std::string item_size;
+  if (item_size_number == 1) {
+    item_size = "small";
+  } else if (item_size_number == 2) {
+    item_size = "medium";
+  } else if (item_size_number == 3) {
+    item_size = "large";
+  } else {
+    item_size = "small";
+  }
+  outFile << item_size << std::endl;
+  outFile << pasta.get_pasta_sauce() << std::endl;
+  int no_ingredients = pasta.get_ingredient_list().size();
+  outFile << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    outFile << pasta.get_ingredient(i).get_name() << std::endl;
+  }
+
+  outFile.close();
 }
 
 // deconstructor
--- WriteIn.h
+++ WriteIn.h
@@ -5,6 +5,12 @@
 
 #include "MenuItem.h"
 #include "IngredientList.h"
+#include "Pasta.h"
+#include "Pizza.h"
+#include "Burger.h"
+#include "Noodle.h"
+#include "Chips.h"
+#include "Rice.h"
 
 
 class WriteIn {
@@ -20,6 +26,30 @@ class WriteIn {
 
   // write to ingredient list text file
   void write_ingredient_list(IngredientList ingredient_list);
+
+  // write in menu item
+  void write_menu_item(MenuItem* menu_item, std::ofstream* outfile);
+
+  // write in pasta
+  void write_pasta(Pasta pasta);
+
+  // write in pizza
+  void write_pizza(Pizza pizza);
+
+    // write in burger
+  void write_burger(Burger burger);
+
+    // write in noodle
+  void write_noodle(Noodle noodle);
+
+    // write in chips
+  void write_chips(Chips chips);
+
+    // write in rice
+  void write_rice(Rice rice);
+
+    // write in drink
+  void write_drink(Drink drink);
 
   // deconstructor
   ~WriteIn();
