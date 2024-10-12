65f280e5283e7781f58e810698a5f15b6c24a523
Max Howard <max@maxs-mbp.home>
2024-10-13 01:16:45 +1030

    max

--- WriteIn.cpp
+++ WriteIn.cpp
@@ -15,8 +15,15 @@ void WriteIn::write_menu_item_list(std::vector<string> list,
   }
 }
 
-void WriteIn::write_menu_item_list(std::vector<MenuItem> list,
+void WriteIn::write_menu_item_list(std::vector<MenuItem*> list,
                                    std::string name) {  // by menu item vector
+                                     std::string file_name = name + ".txt";
+  std::ofstream outFile(file_name);
+
+  int list_length = (list).size();
+  for (int i = 0; i < list_length; i++) {
+    outFile << list[0][i].get_item_name() << std::endl;
+  }
 }
 
 // write to ingredient list text file
@@ -81,6 +88,87 @@ void WriteIn::write_pizza(Pizza pizza) {
   for (int i = 0; i < no_ingredients; i++) {
     out_file << pizza.get_ingredient(i).get_name() << std::endl;
   }
+
+  out_file.close();
+}
+
+// write in burger
+void WriteIn::write_burger(Burger burger) {
+  std::string file_name = burger.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&burger, &out_file);
+
+  out_file << burger.get_burger_bun() << std::endl;
+  out_file << burger.get_burger_meat() << std::endl;
+  int no_ingredients = burger.get_ingredient_list().size();
+  out_file << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    out_file << burger.get_ingredient(i).get_name() << std::endl;
+  }
+
+  out_file.close();
+}
+
+// write in noodle
+void WriteIn::write_noodle(Noodle noodle) {
+  std::string file_name = noodle.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&noodle, &out_file);
+
+  out_file << noodle.get_noodle_type() << std::endl;
+  out_file << noodle.get_noodle_soup() << std::endl;
+  int no_ingredients = noodle.get_ingredient_list().size();
+  out_file << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    out_file << noodle.get_ingredient(i).get_name() << std::endl;
+  }
+
+  out_file.close();
+}
+
+// write in chips
+void WriteIn::write_chips(Chips chips) {
+  std::string file_name = chips.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&chips, &out_file);
+
+  out_file << chips.get_chips_cut() << std::endl;
+  out_file << chips.get_chips_seasoning() << std::endl;
+  int no_ingredients = chips.get_ingredient_list().size();
+  out_file << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    out_file << chips.get_ingredient(i).get_name() << std::endl;
+  }
+
+  out_file.close();
+}
+
+// write in rice
+void WriteIn::write_rice(Rice rice) {
+  std::string file_name = rice.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&rice, &out_file);
+
+  out_file << rice.get_rice_type() << std::endl;
+  int no_ingredients = rice.get_ingredient_list().size();
+  out_file << no_ingredients << std::endl;
+  for (int i = 0; i < no_ingredients; i++) {
+    out_file << rice.get_ingredient(i).get_name() << std::endl;
+  }
+
+  out_file.close();
+}
+
+// write in drink
+void WriteIn::write_drink(Drink drink) {
+  std::string file_name = drink.get_item_name() + ".txt";
+  std::ofstream out_file(file_name);  // make write file object
+
+  write_menu_item(&drink, &out_file);
 
   out_file.close();
 }
