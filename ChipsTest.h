b83bee455ab0a3416818289da7ce3f0f2823e7e9
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-13 09:55:03 +1030

    A

--- ChipsTest.h
+++ ChipsTest.h
@@ -4,6 +4,8 @@ class ChipsTest {
 private:
     // default test case for chips
     void test_chips_default() {
+        // Create a chips via the default constructor, 
+        // then compare the values against the expected cut/seasoning/name/price/size
         Chips c;
         if (c.get_chips_cut() != "N/A") {
             cout << "default test for chips cut failed, result unexpected" << endl;
@@ -24,6 +26,8 @@ private:
 
     // test case 1 for chips, no seasoning
     void test_chips_case1() {
+        // Create a chips with parameters (excluding ingredient), 
+        // then compare the values against the expected cut/seasoning/name/price/size
         Chips c1 = Chips("chips1", 3.5, 1, "cut1", "seasoning1");
         if (c1.get_chips_cut() != "cut1") {
             cout << "test 1 for chips cut failed, result unexpected" << endl;
@@ -44,23 +48,10 @@ private:
 
     // test case 2 for chips, with 1 ingredient
     void test_chips_case2() {
+        // Create a chips with parameters (including 1 ingredient), 
+        // then compare the ingredient and final price against the expected ingredient/price
         Ingredient i1 = Ingredient("i1", 0.2);
         Chips c2 = Chips("chips2", 5, 2, "cut2", "seasoning2", {i1});
-        if (c2.get_chips_cut() != "cut2") {
-            cout << "test 2 for chips cut failed, result unexpected" << endl;
-        }
-        if (c2.get_chips_seasoning() != "seasoning2") {
-            cout << "test 2 for chips seasoning failed, result unexpected " << endl;
-        }
-        if (c2.get_item_name() != "chips2") {
-            cout << "test 2 for chips name failed, result unexpected" << endl;
-        }
-        if (c2.get_item_price() != 5) {
-            cout << "test 2 for chips price failed, result unexpected" << endl;
-        }
-        if (c2.get_item_size() != 2) {
-            cout << "test 2 for chips size failed, result unexpected" << endl;
-        }
         if (c2.get_ingredient(0).get_name() != "i1") {
             cout << "test 2 for rice ingredient 1 name failed, result unexpected" << endl;
         }
@@ -74,6 +65,9 @@ private:
 
     // Test case 3 for chips, with multiple ingredients, and setting values for variables
     void test_chips_case3() {
+        // Create a chips with parameters (including multiple ingredients), 
+        // change the parameters with set functions, 
+        // then compare the parameters, ingredients, and final price against the expected ingredient/price
         Ingredient i1 = Ingredient("i1", 0.2);
         Ingredient i2;
         Ingredient i3 = Ingredient("ingredient3", 0.8);
@@ -118,6 +112,7 @@ private:
         }
     }
 public:
+    // run_tests runs the test cases created above 
     void run_tests() {
         test_chips_default();
         test_chips_case1();
