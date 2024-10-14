f194919595e501686a4ef7ecdc319f9f4125fa46
Yu Tong Wu <a1947301@sa.edu.au>
2024-10-14 14:02:04 +1030

    A

--- CategoriesFood.h
+++ CategoriesFood.h
@@ -3,11 +3,16 @@
 
 class CategoriesFood {
  private:
-  int state_categories_food = 0;  // 0 mean no need to run portal again, 1 means
-                                  // need to run again.
+  // 0 mean no need to run portal again, 1 means need to run again.
+  int state_categories_food = 0;  
  public:
+  // runs the portal
   void run(int& state_portal_food);
+
+  // return the state of portal (0 or 1)
   int get_state_categories_food();
+
+  // default constructor/destructor
   CategoriesFood();
   ~CategoriesFood();
 };
